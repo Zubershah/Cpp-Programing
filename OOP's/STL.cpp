a7a4fc34401a8af3f4e0c495d42f9d0b482b6f78
Zubershah <engr.zubershah@gmail.com>
2025-12-23 23:18:25 +0530

    Added file No. 60

---
+++ OOP's/STL.cpp
@@ -0,0 +1,26 @@
+/*
+Components of STL
+Conatiners: Object which stores data
+Algorithm: Procedure to process data
+Iterators: Object which points to an element of a container
+
+Conatainer types:
+1. Sequence
+    1. Vector
+    2. List
+    3. Dequeue
+2. Associative
+    1. Map / Multimap
+    2. Set / Multiset
+3. Derived
+    1. Stack
+    2. Queue
+    3. Priority Queue
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
